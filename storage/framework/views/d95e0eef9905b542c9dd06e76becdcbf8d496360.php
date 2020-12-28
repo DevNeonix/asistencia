a1e8d47a802726d8d9bc8b3267ed661e7563b97d
Jolver André Gil Quevedo <devneonix@gmail.com>
2020-12-28 12:28:02 -0500

    add

--- storage/framework/views/d95e0eef9905b542c9dd06e76becdcbf8d496360.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    Bienvenido
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/asistencia/resources/views/pages/home.blade.php ENDPATH**/ ?>
